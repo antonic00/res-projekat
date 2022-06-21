52c742df3dcd75c94ea1250a069137c9aa9b560a
antonic00 <101349979+antonic00@users.noreply.github.com>
2022-06-21 16:24:07 +0200

    Pakovanje primljenog sadržaja od LoadBalancera, u svoju internu strukturu

--- worker.py
+++ worker.py
@@ -1,7 +1,12 @@
 import socket
+import pickle
+from collection_description import Collection_Description
+
+from description import Description
+from historical_collection import Historical_Collection
 
 class Worker:
-    def __init__(self):
+    def _init_(self):
         self.worker_sa_load_balancerom = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     
     def konektuj_sa_load_balancerom(self):
@@ -10,25 +15,26 @@ class Worker:
             self.worker_sa_load_balancerom.listen(4)
             print("Slusam...")
             konekcija, adresa = self.worker_sa_load_balancerom.accept()
-            
+            print("Sve konektovano.")
+            while(True):
+                data = konekcija.recv(4096)
+                buffer = pickle.loads(data)
+                for description in buffer:
+                    collection_description = Collection_Description(description.id, description.dataset)
+                    for item in description.lista_itema :
+                        collection_description.historical_collection.dodaj_u_niz_propertija(item[0], item[1]) 
+                        print(collection_description.id)
+                        print(collection_description.dataset)
+                        for item in collection_description.historical_collection.worker_properties:
+                            print(item[0])
+                            print(item[1])
         except socket.error:
             print("Neuspesna konekcija sa loadBalancerom")
             return False
-        return True
+        
 
-    def odredi_data_set(self, code):
-        if code in ["CODE_ANALOG", "CODE_DIGITAL"]:
-            return 1
-        if code in ["CODE_CUSTOM", "CODE_LIMITSET"]:
-            return 2
-        if code in ["CODE_SINGLENODE", "CODE_MULTIPLENODE"]:
-            return 3
-        if code in ["CODE_CONSUMER", "CODE_SOURCE"]:
-            return 4
+    
 
-if __name__ == "__main__":
+if _name_ == "_main_":
     worker = Worker()
-    if worker.konektuj_sa_load_balancerom():
-        print("Sve konektovano.")
-        input()
-         
+    worker.konektuj_sa_load_balancerom()
