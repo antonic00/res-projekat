fcb8d5c6e8bcfc6164e1068d550bfffcc76812c1
borislavtubic <101349967+borislavtubic@users.noreply.github.com>
2022-06-22 13:29:46 +0200

    Pakovanje po određenom datasetu, i upis u bazu podataka.

--- worker.py
+++ worker.py
@@ -1,34 +1,60 @@
 import socket
+import pickle
+from database_connection import konekcija
+from datetime import datetime
+import mysql.connector
+
+from collection_description import Collection_Description
+
 
 class Worker:
     def __init__(self):
         self.worker_sa_load_balancerom = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    
+        self.baza = None
     def konektuj_sa_load_balancerom(self):
         try:
-            self.worker_sa_load_balancerom.bind((socket.gethostname(), 1001))
-            self.worker_sa_load_balancerom.listen(4)
-            print("Slusam...")
-            konekcija, adresa = self.worker_sa_load_balancerom.accept()
-            
+            self.worker_sa_load_balancerom.connect((socket.gethostname(), 8001))
         except socket.error:
             print("Neuspesna konekcija sa loadBalancerom")
             return False
         return True
+ 
+    def primi_podatke(self):
+        self.baza = konekcija()
+        while(True):
+            data = self.worker_sa_load_balancerom.recv(4096)
 
-    def odredi_data_set(self, code):
-        if code in ["CODE_ANALOG", "CODE_DIGITAL"]:
-            return 1
-        if code in ["CODE_CUSTOM", "CODE_LIMITSET"]:
-            return 2
-        if code in ["CODE_SINGLENODE", "CODE_MULTIPLENODE"]:
-            return 3
-        if code in ["CODE_CONSUMER", "CODE_SOURCE"]:
-            return 4
+            try:
+                buffer = pickle.loads(data)
+            except EOFError:
+                continue
+            if(buffer == "exit"):
+                self.worker_sa_load_balancerom.close()
+                exit()
+            collection_description = Collection_Description(buffer.id, buffer.dataset)
+            for item in buffer.lista_itema :
+                collection_description.historical_collection.dodaj_u_niz_propertija(item[0], item[1]) 
+                print(collection_description.id)
+                print(collection_description.dataset)
+                for item in collection_description.historical_collection.worker_properties:
+                    timestamp = datetime.now()
+                    self.dodaj_element(collection_description.id, collection_description.dataset, item[0], item[1], timestamp)
+     
+
+    def dodaj_element(self, id, dataset, code, value, timestamp):
+        my_cursor = self.baza.cursor()
+        if(dataset == 1):
+            my_cursor.execute("INSERT INTO DATASET_1(id, dataset, code, value, timestamp) VALUES (%s, %s, %s, %s, %s)", (id, dataset, code, value, timestamp))
+        if(dataset == 2):
+            my_cursor.execute("INSERT INTO DATASET_2(id, dataset, code, value, timestamp) VALUES (%s, %s, %s, %s, %s)", (id, dataset, code, value, timestamp))
+        if(dataset == 3):
+            my_cursor.execute("INSERT INTO DATASET_3(id, dataset, code, value, timestamp) VALUES (%s, %s, %s, %s, %s)", (id, dataset, code, value, timestamp))
+        if(dataset == 4):
+            my_cursor.execute("INSERT INTO DATASET_4(id, dataset, code, value, timestamp) VALUES (%s, %s, %s, %s, %s)", (id, dataset, code, value, timestamp))
+        self.baza.commit()
 
 if __name__ == "__main__":
     worker = Worker()
     if worker.konektuj_sa_load_balancerom():
-        print("Sve konektovano.")
-        input()
-         
+        print("uspesna konekcija sa LB.\n")
+        worker.primi_podatke()
