b668cb03347ab4b95ea14440690ab53e3d52d7eb
antonic00 <101349979+antonic00@users.noreply.github.com>
2022-06-22 22:10:49 +0200

    Omogućavanje čitanja podataka iz baze i ispis tih podataka

--- reader.py
+++ reader.py
@@ -1,10 +1,13 @@
+import pickle
 from socket import socket
+from _thread import *
+from database_functions import dobavi_podatak, konekcija
 import socket
 
 class Reader:
     def __init__(self):
         self.reader_to_worker = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    
+        self.baza = None
     def konektuj_sa_workerom(self):  
         try:
             soket, adresa = self.reader_to_worker.accept()
@@ -13,15 +16,71 @@ class Reader:
             return False
         print("Konektovao se sa Workerom.")
         return soket
+    
+    def primi_podatke(self, soket):
+        self.baza = konekcija()
+        while(True):
+            data = soket.recv(4096)
+            try:
+                buffer = pickle.loads(data)
+            except EOFError:
+                continue
+
+            kod = buffer[0]
+            vrednost = buffer[1]
+
+            rezultat = self.citanje_iz_baze(self.baza, kod)
+
+            '''if kod in ["CODE_ANALOG", "CODE_DIGITAL"]:
+                rezultat = self.citanje_iz_baze(kod, self.baza, 'baza_podataka.dataset_1', 'dataset_1.code')
+            if kod in ["CODE_CUSTOM", "CODE_LIMITSET"]:
+                rezultat = self.citanje_iz_baze(kod, self.baza, 'baza_podataka.dataset_2', 'dataset_2.code')
+            if kod in ["CODE_SINGLENODE", "CODE_MULTIPLENODE"]:
+                rezultat = self.citanje_iz_baze(kod, self.baza, 'baza_podataka.dataset_3', 'dataset_3.code')
+            if kod in ["CODE_CONSUMER", "CODE_SOURCE"]:
+                rezultat = self.citanje_iz_baze(kod, self.baza, 'baza_podataka.dataset_4', 'dataset_4.code')'''
+            
+            for line in rezultat:
+                print(line)
+
 
     def priprema_soketa(self):
             self.reader_to_worker.bind((socket.gethostname(), 9000))
             self.reader_to_worker.listen(4)
+    
+    def citanje_iz_baze(self, baza, kod):
+        my_cursor = baza.cursor()
+        lista = list()
+        lista.append(kod)
+        my_cursor.execute("SELECT * FROM baza_podataka.dataset_1 WHERE dataset_1.code = %s", lista)
+        rezultat = my_cursor.fetchall()
+        if len(rezultat) != 0:
+            return rezultat
 
+        my_cursor.execute("SELECT * FROM baza_podataka.dataset_2 WHERE dataset_2.code = %s", lista)
+        rezultat = my_cursor.fetchall()
+        if len(rezultat) != 0:
+            return rezultat
 
+        my_cursor.execute("SELECT * FROM baza_podataka.dataset_3 WHERE dataset_3.code = %s", lista)
+        rezultat = my_cursor.fetchall()
+        if len(rezultat) != 0:
+            return rezultat
+
+        my_cursor.execute("SELECT * FROM baza_podataka.dataset_4 WHERE dataset_4.code = %s", lista)
+        rezultat = my_cursor.fetchall()
+        return rezultat
 
 if __name__ == "__main__":
     reader = Reader()
     reader.priprema_soketa()
     while(True):
         soket = reader.konektuj_sa_workerom()
+        start_new_thread(reader.primi_podatke, (soket, ))
+    
+    '''
+        if reader.konektuj_sa_workerom():
+        print("uspesna konekcija sa Workerom.\n")
+        reader.primi_podatke()
+    '''
+
